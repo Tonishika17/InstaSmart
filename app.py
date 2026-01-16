987bce8955a230c870f3fd52bd24178d84414245
Tonishika <tonishika17@gmail.com>
2026-01-16 20:56:59 +0530

    Create,link html form

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-    return "Your Instagram Reach"
+    if request.method=="POST":
+        followers = request.form.followers
+        likes = request.form.likes
+        comments = request.form.comments
+
+        engagement_rate = (likes+comments/followers)*100
+
+        return render_template("index.html",Engagement=engagement_rate)
+
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
