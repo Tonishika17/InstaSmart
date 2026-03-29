4ae535363fb4ca5d0e32c50e6389af1fbffeb25e
Tonishika <tonishika17@gmail.com>
2026-03-29 15:08:31 +0530

    Add Randomforestregressor

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 
 data = pd.read_csv("dataset.csv")
@@ -11,7 +12,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+#model = LinearRegression()
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 predictions=model.predict(X_test)
 
